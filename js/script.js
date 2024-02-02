a0ab2ba839724257295644bb54eaf2540e6a6b0d
jlandrews87 <155691210+jlandrews87@users.noreply.github.com>
2024-02-02 11:06:47 -0500

    Complete Rock Paper Scissors Project
    I finished the project. I will hold on to this version of the project
    until The Odin Project calls for me to go back to it. Instead of
    obsessing over small details and getting lost in the weeds, I think it
    would be better to keep moving forward and learning new things.

--- js/script.js
+++ js/script.js
@@ -9,35 +9,38 @@ function getComputerChoice() {
     }
 }
 function playGame() {
-function playRound(playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
-        if (playerSelection === "rock" && computerSelection === "Rock") {
-            return "It's a tie. Rock does not beat rock."
-        } else if (playerSelection === "rock" && computerSelection === "Paper") {
-            return "You lose :( Paper covers rock.";
-        } else if (playerSelection === "rock" && computerSelection === "Scissors") {
-            return "You win! Rock smashes scissors!";
-        } else if (playerSelection === "paper" && computerSelection === "Rock") {
-            return "You win! Paper covers rock!";
-        } else if (playerSelection === "paper" && computerSelection === "Paper") {
-            return "It's a tie. Paper does not beat paper.";   
-        } else if (playerSelection === "paper" && computerSelection === "Scissors") {
-            return "You lose :( Scissors cut paper.";
-        } else if (playerSelection === "scissors" && computerSelection === "Rock") {
-            return "You lose :( Rock smashes scissors.";
-        } else if (playerSelection === "scissors" && computerSelection === "Paper") {
-            return "You win! Scissors cut paper!";
-        } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
-            return "It's a tie. Scissors does not beat scissors."; 
-        } else return "Please choose either rock, paper, or scissors";
-    }
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection));
-    console.log(playRound(playerSelection,computerSelection));
+    const computerSelection = getComputerChoice();
+    function playRound(playerSelection, computerSelection) {
+            playerSelection = playerSelection.toLowerCase();
+            if (playerSelection === "rock" && computerSelection === "Rock") {
+                return "It's a tie. Rock does not beat rock."
+            } else if (playerSelection === "rock" && computerSelection === "Paper") {
+                return "You lose :( Paper covers rock.";
+            } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+                return "You win! Rock smashes scissors!";
+            } else if (playerSelection === "paper" && computerSelection === "Rock") {
+                return "You win! Paper covers rock!";
+            } else if (playerSelection === "paper" && computerSelection === "Paper") {
+                return "It's a tie. Paper does not beat paper.";   
+            } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+                return "You lose :( Scissors cut paper.";
+            } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+                return "You lose :( Rock smashes scissors.";
+            } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+                return "You win! Scissors cut paper!";
+            } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+                return "It's a tie. Scissors does not beat scissors."; 
+            } else return "Please choose either rock, paper, or scissors";
+        }   console.log(playRound(playerSelection,computerSelection));
 }
 
-let playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-playGame();
+let playerSelection = prompt("Paper, Rock, or Scissors?");
+
+let i = 0;
+do {
+    i += 1;
+    console.log(playGame());
+} while (i < 5);
+
+
+
